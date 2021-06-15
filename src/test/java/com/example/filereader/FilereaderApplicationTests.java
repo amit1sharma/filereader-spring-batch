4bdc6c4888440ff72aa2079b29ecca13b556030e
amit1sharma <funkiamit@gmail.com>
2021-06-16 00:29:32 +0400

    path property used in test

--- src/test/java/com/example/filereader/FilereaderApplicationTests.java
+++ src/test/java/com/example/filereader/FilereaderApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.filereader;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
@@ -25,12 +26,15 @@ class FilereaderApplicationTests {
     @Autowired
     private WebApplicationContext webApplicationContext;
 
+    @Value("${upload.folder.path}")
+    private String path;
+
     @Test
     void contextLoads() throws Exception {
         byte[] buffer = "fileName,fileType,fileSize\n".getBytes();
         int number_of_lines = 400000;
 
-        FileChannel rwChannel = new RandomAccessFile(new File("/home/yamraaj/Downloads/upload_path/test.txt"), "rw").getChannel();
+        FileChannel rwChannel = new RandomAccessFile(new File(path+"test.txt"), "rw").getChannel();
         ByteBuffer wrBuf = rwChannel.map(FileChannel.MapMode.READ_WRITE, 0, buffer.length * number_of_lines);
         for (int i = 0; i < number_of_lines; i++)
         {
@@ -39,7 +43,7 @@ class FilereaderApplicationTests {
         }
         rwChannel.close();
 
-        InputStream is = new FileInputStream(new File("/home/yamraaj/Downloads/upload_path/test.txt"));
+        InputStream is = new FileInputStream(new File(path+"test.txt"));
         MockMultipartFile file = new MockMultipartFile( "file", "csv.txt",MediaType.APPLICATION_JSON_VALUE, is);
         MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
         mockMvc.perform(multipart("/upload/file").file(file))
